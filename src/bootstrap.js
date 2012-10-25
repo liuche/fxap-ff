08d9d707581db270d5b86897a3178e786f729032
Chenxia Liu <liuche@mozilla.com>
2012-10-24 18:03:44 -0700

    Add menu item with cleanup.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,17 +4,53 @@
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
+const NS_FXACCT = "FxAcctNS";
+
+const fxacctId = "menu_ToolsFxAcctItem";
 
 function install(aData, aReason) {}
 
+// Window listener for new windows.
+let windowListener = {
+  onOpenWindow: function(win) {
+    let domWindow = win.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      addToWindow(domWindow);
+    }, false);
+  },
+  onCloseWindow: function(win) {},
+  onWindowTitleChange: function(win, title) {}
+};
+
+// Setup (startup, enable, install): insert add-on into browser UI, register any
+// listeners, etc.
 function startup(aData, aReason) {
-  // Setup (startup, enable, install): insert add-on into browser UI, register any
-  // listeners, etc.
-  dump("Starting up! Hello world!\n");
+  // Add menu-item to existing windows.
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+             .getService(Ci.nsIWindowMediator);
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    addToWindow(domWindow);
+    // TODO: [optional] Add menu item for Win7 windows.
+  }
+  wm.addListener(windowListener);
+}
+
+// Add Firefox Account menu item to a window.
+function addToWindow(win) {
+  dump("INFO: function addToWindow\n");
+  let fxAcctMI= win.document.createElementNS(NS_FXACCT, "menuitem");
+  // TODO: Display different text based on account existence.
+  fxAcctMI.setAttribute("label", "Firefox Account");
+  fxAcctMI.setAttribute("id", fxacctId);
+  // TODO: Hook up chrome tab launch-js.
+  win.document.getElementById("menu_ToolsPopup").insertBefore(fxAcctMI, win.document.getElementById("devToolsSeparator"));
 }
 
 function shutdown(aData, aReason) {
-  dump("Shutting down!\n");
+  dump("INFO: Shutting down FxAcct add-on!\n");
   // When the application is shutting down we normally don't have to clean
   // up any UI changes made
   if (aReason == APP_SHUTDOWN)
@@ -22,6 +58,19 @@ function shutdown(aData, aReason) {
 
   // Clean-up (shutdown, disable, uninstall): remove elements from browser, unregister
   // any listeners, etc.
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+             .getService(Ci.nsIWindowMediator);
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+  wm.removeListener(windowListener);
+}
+
+function unloadFromWindow(win) {
+  let menuItem = win.document.getElementById(fxacctId);
+  menuItem && menuItem.parentNode.removeChild(menuItem);
 }
 
 function uninstall(aData, aReason) {}
