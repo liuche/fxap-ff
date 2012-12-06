76fb11c155d18b2f14a2a1b33ba5f0fb819973de
Chenxia Liu <liuche@mozilla.com>
2012-12-06 14:50:00 -0800

    Add flow-map for progressing through screens.

--- src/modules/FxAccountSetup.js
+++ src/modules/FxAccountSetup.js
@@ -15,58 +15,88 @@ const CONFIRM_PAGE = 5;
 
 const pageIds = ["InitialSigninPage", "BasicSigninPage", "AdvancedSigninPage", "SuccessPage", "CreateAccountPage", "ConfirmAccountPage"];
 
+const flowGraph = {"buttonNewAccount" : CREATE_PAGE,
+                   "buttonExistingAcount" : BASIC_SIGNIN,
+                   "linkAdvSignin" : ADV_SIGNIN,
+                   "buttonSignIn" : 3,
+                   "linkForgotPassword" : 0,
+                   "linkCreateAccount" : CREATE_PAGE,
+                   "buttonSigninAdv" : ADV_SIGNIN,
+                   "buttonSignout" : 0,
+                   "linkSignIn" : BASIC_SIGNIN,
+                   "buttonCreate" : CONFIRM_PAGE
+                  };
+
 let window;
 let fxAccountSetup;
 
 function main() {
-  dump("main()\n");
+  dump("new fxAccountSetup\n");
   // Launch FxAccount Sign-In screen.
   window = openAndReuseOneTab("chrome://fxacct/content/sign-in.xul");
-  dump("new fxAccountSetup\n");
   fxAccountSetup = new FxAccountSetup();
   fxAccountSetup.initListeners(window);
 }
 
 this.FxAccountSetup = function FxAccountSetup() {
   this.window = null;
+  this.selectedTab = null;
   this.currentPage = null;
-}
-
-FxAccountSetup.prototype = {
-  initListeners: function init(window) {
-    dump("initListeners()\n");
-    this.window = window;
-    this.window.gBrowser.addEventListener("load", function() {
-      dump("loaded!\n");
-    });
-    dump("document: " + this.window.document + "\n");
-    _addListeners(this.window);
-  },
-  _addListeners: function _addListeners(win) {
-    win.addEventListener("
-  },
-  onclick: function onclick(event) {
-    dump("received onClick!\n");
-  },
-  signIn: function signIn() {
-    dump("signin()\n");
-    this._showPage(BASIC_SIGNIN);
-  },
-  newAccount: function newAccount() {
-    dump("newAccount()\n");
-    dump(this.document.getElementById(pageIds[BASIC_SIGNIN]).collapsed);
-    this._showPage(CREATE_PAGE);
-  },
-  _showPage: function _showPage(index) {
-    this.currentPage.collapsed = true;
-    this.currentPage = this.document.getElementById(pageIds[index]);
-    this.currentPage.collapsed = false;
-  },
-  test: function test() {
-    dump("test\n");
-  }
 };
 
+FxAccountSetup.prototype = {
+  init: function init(window) {
+    this.window = window;
+    this.addListeners();
+  },
+  onPageLoaded: function onPageLoaded(aEvent) {
+    dump("onload callback\n");
+    // Set current page.
+    dump("testing window doc: " + window.gBrowser.contentDocument + "\n");
+    this.currentPage = window.gBrowser.contentDocument.getElementById(pageIds[0]);
+    this.selectedTab = window.gBrowser.selectedTab;
+    dump("currentPage: " + this.currentPage + "\n");
+    dump("selectedTab:" + this.selectedTab + "\n");
+    dump("onLoad done\n");
+  },
+  addListeners: function addListeners() {
+    dump("initListeners()\n");
+    window.gBrowser.selectedTab.addEventListener("pageshow", this.onPageLoaded.call(this), false);
+    dump("done\n");
+  },
+  onclick: function onclick(element) {
+    dump(element.id + "clicked!\n");
+    let doc = window.gBrowser.contentDocument;
+    dump("contentDoc: " + doc + "\n");
+  },
+  signIn: function signIn(element) {
+    dump("signin() ");
+    dump(element.getAttribute("id") + "\n");
+    dump("dumped\n");
+    this._displayPage(BASIC_SIGNIN);
+  },
+  newAccount: function newAccount(event) {
+    dump("newAccount() ");
+    dump(event.name + "\n");
+    dump("dumped\n");
+    dump(this.document.getElementById(pageIds[BASIC_SIGNIN]).collapsed);
+    this._displayPage(CREATE_PAGE);
+  },
+  _displayPage: function _displayPage(index) {
+    // hack - onload and pageshow callbacks fire before elements are created.
+    if (this.currentPage == null) {
+     this.currentPage = window.gBrowser.contentDocument.getElementById(pageIds[0]);
+    }
+    dump("getting " + pageIds[index] + "\n");
+    this.currentPage.collapsed = true;
+    this.currentPage = window.gBrowser.contentDocument.getElementById(pageIds[index]);
+    this.currentPage.collapsed = false;
+    dump("showing page " + index + "\n");
+  },
+};
+function test() {
+  dump("calling test() function\n");
+}
 /**
  * Reuse tab containing the URL if it exists; otherwise, open in a new tab.
  * Returns the browser window containing new tab, or null if no windows exist.
